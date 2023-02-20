de2ca116c33db97479f985d39ac1f02b82365876
Sergey Grigorev <grigoscool@mail.ru>
2023-02-20 15:15:19 +0300

    create 3 models

--- music_api/models.py
+++ music_api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Musician(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    musician = models.ForeignKey(Musician, on_delete=models.PROTECT)
+    date_release = models.DateField()
+
+    def __str__(self):
+        return f'Album {self.title} of {self.musician}'
+
+
+class Track(models.Model):
+    album = models.ForeignKey(
+        Album, on_delete=models.DO_NOTHING, null=True)
+    title = models.CharField(max_length=100)
+    serial_number = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.title
